162d1b9befd076c89b9a6101f93a04f074f9f72b
Anders Kirkeby <anders@klever-kirkeby.com>
2022-12-03 15:02:04 +0100

    refactor: use char extension to calculate priority

--- src/main/kotlin/solutions/Day03.kt
+++ src/main/kotlin/solutions/Day03.kt
@@ -2,11 +2,10 @@ package solutions
 
 import models.InputProvider
 
-val PRIORITIES = ('a'..'z') + ('A'..'Z')
-
 context (InputProvider)
 class Day03 : Day(3, 2022, "Rucksack Reorganization") {
     private val rucksacks: List<CharArray> = input.map(String::toCharArray)
+
 
     @OptIn(ExperimentalStdlibApi::class)
     override fun part1(): Int {
@@ -19,20 +18,22 @@ class Day03 : Day(3, 2022, "Rucksack Reorganization") {
 
         return compartments
             .map { (first, second) -> first.first(second::contains) }
-            .sumOf { PRIORITIES.indexOf(it) + 1 }
+            .sumOf(Char::toPriority)
     }
 
     override fun part2(): Any {
-        return rucksacks.map { it.toSet() }.chunked(3)
-            .fold(0) { result, group ->
-                // The badge is the only common char between the three rucksacks
-                val badge = group.reduce { acc, chars -> acc.intersect(chars) }.first()
-
-                // Add the group value to the result
-                result + PRIORITIES.indexOf(badge) + 1
+        return rucksacks
+            .map { it.toSet() }
+            .chunked(3)
+            .sumOf { group ->
+                group.reduce { acc, chars -> acc.intersect(chars) }
+                    .first().toPriority()
             }
     }
 }
+
+private fun Char.toPriority() = this - (if (isUpperCase()) 'A' - 27 else 'a' - 1)
+
 
 fun main() {
     test<Day03>(157, 70)
