9302dc366348892b8eecd346de8a05c32d3422e4
Anders Kirkeby <anders@klever-kirkeby.com>
2022-12-04 10:01:33 +0100

    feat: add solution to day 4

---
+++ src/main/kotlin/solutions/Day04.kt
@@ -0,0 +1,39 @@
+package solutions
+
+import models.InputProvider
+
+context (InputProvider)
+class Day04 : Day(4, 2022, "Camp Cleanup") {
+    private val assignments: List<Pair<IntRange, IntRange>> = input.map { line ->
+        line.split(",").let {
+            it[0].toRange() to it[1].toRange()
+        }
+    }
+
+
+    override fun part1(): Any {
+        return assignments.count { (first, second) ->
+            first.fullyContains(second) || second.fullyContains(first)
+        }
+    }
+
+    override fun part2(): Any {
+        return assignments.count { (first, second) ->
+            first.overlaps(second)
+        }
+    }
+}
+
+fun String.toRange(): IntRange =
+    this.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) }
+
+fun IntRange.fullyContains(other: IntRange): Boolean =
+    this.first <= other.first && this.last >= other.last
+
+fun IntRange.overlaps(other: IntRange): Boolean =
+    this.any { it in other }
+
+fun main() {
+    test<Day04>(2, 4)
+    solve<Day04>()
+}
