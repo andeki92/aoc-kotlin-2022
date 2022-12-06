aed6f174cd56918ae27cf1d140c02c831ae15a55
Anders Kirkeby <anders@klever-kirkeby.com>
2022-12-06 07:17:57 +0100

    feat: add solution to day 6

---
+++ src/main/kotlin/solutions/Day06.kt
@@ -0,0 +1,22 @@
+package solutions
+
+import models.InputProvider
+
+
+context (InputProvider)
+class Day06 : Day(6, 2022, "Tuning Trouble") {
+
+    private val signal = input.first()
+
+    override fun part1(): Int = signal.indexOfFirstUniqueWindow(4)
+    override fun part2(): Int = signal.indexOfFirstUniqueWindow(14)
+
+    private fun String.indexOfFirstUniqueWindow(window: Int): Int =
+        this.windowed(window).indexOfFirst { it.toSet().size == window } + window
+}
+
+
+fun main() {
+    test<Day06>(7, 19)
+    solve<Day06>()
+}
